9dd6464c8a91e514d15a3360402ec6eb0a30742e
Stanislav Khrapov <stanislav.khrapov@chintai.io>
2023-02-07 13:29:07 +0100

    Add benchmark to github action

--- benchmark.sh
+++ benchmark.sh
@@ -4,7 +4,8 @@
 pytest tests/test_matching_engine.py::TestMatchingEngine::test_matching_with_benchmark \
   --dist no \
   --benchmark-enable \
-  --benchmark-autosave
+  --benchmark-autosave \
+  --benchmark-json=benchmark.json
 
 # Compare saved results
 pytest-benchmark compare
